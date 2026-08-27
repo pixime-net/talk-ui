47f5a80d1abc40677cdd6377e42060ba4fd32680
Xavier Thomas <xvthomas@gmail.com>
2026-08-27 19:07:47 +0200

    fix(sonar): adjust // NOSONAR position

--- src/config/agent.ts
+++ src/config/agent.ts
@@ -1,8 +1,7 @@
 import { HttpAgent } from "@ag-ui/client";
 import { config } from "./env";
 
-// NOSONAR: input is a short config URL, pathological backtracking cannot occur
-const normalizedAgentURL = config.VITE_AGENT_URL.replace(/\/+$/, "");
+const normalizedAgentURL = config.VITE_AGENT_URL.replace(/\/+$/, ""); // NOSONAR: input is a short config URL, pathological backtracking cannot occur
 const talkAgent = new HttpAgent({ url: `${normalizedAgentURL}/agent` });
 
 export const agents = { default: talkAgent } as const;
